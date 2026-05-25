042783fa2a27b42d14965a0fd3d92ceec8de4899
tonijoki <133871089+tonijoki@users.noreply.github.com>
2026-05-25 08:03:43 +0300

    Add renamed Exam Calculator plugin files

---
+++ renderer.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Renderer for the exam calculator block.
+ *
+ * @package    block_exam_calculator
+ * @copyright  2024 Toni Jokinen
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Renderer implementation.
+ */
+class block_exam_calculator_renderer extends plugin_renderer_base {
+
+    /**
+     * Render calculator template.
+     *
+     * @param array $data template data
+     * @return string
+     */
+    public function render_calculator(array $data): string {
+        return $this->render_from_template('block_exam_calculator/calculator', $data);
+    }
+}
